4d3e5bcd04a0af3188c16e4e69592727002fc891
Postmodern <postmodern.mod3@gmail.com>
2013-03-05 20:47:13 -0800

    Oops, should be 12 characters, not 13.

--- spec/services/shorl_spec.rb
+++ spec/services/shorl_spec.rb
@@ -7,7 +7,7 @@ describe Services::Shorl do
   its(:action)   { should == '/create.php' }
 
   let(:url)      { "http://www.google.com/#{rand(1024)}" }
-  let(:shorturl) { /^http:\/\/shorl\.com\/[a-z]{13}$/    }
+  let(:shorturl) { /^http:\/\/shorl\.com\/[a-z]{12}$/    }
 
   include_context "Services"
 end
